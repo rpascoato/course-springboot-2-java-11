404c7a6462a0a0617a17f06ce545e3e77242a91a
Raphael Pascoato <rapha367@live.com>
2021-11-02 21:17:41 -0300

    Service layer, component registration

---
+++ src/main/java/com/rpascoato/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.rpascoato.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rpascoato.course.entities.User;
+import com.rpascoato.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
